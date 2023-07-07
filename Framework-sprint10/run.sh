cda8454df44f71a68329cce6018ecf1890503daa
Tcheeen <119050437+Tcheeen@users.noreply.github.com>
2023-07-07 14:30:17 +0200

    Sprint10

--- Framework-sprint10/run.sh
+++
@@ -1,7 +0,0 @@
-cd Framework/build/web/WEB-INF/classes
-jar -cf etu2004Files.jar .
-mv etu2004Files.jar ../../../../../Test-framework/WEB-INF/lib
-cd ../../../../../Test-framework/WEB-INF/classes
-javac -cp ../lib/* -d . *java
-
-  
